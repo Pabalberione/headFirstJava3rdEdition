4626dd7d8a2c3c59f4829a753a9db8f438eaab68
Pabalberione <pabalberione@gmail.com>
2024-05-03 09:08:55 -0300

    Adding more chapter 5 exercises

---
+++ src/main/java/Chapter5/codeMagnets/MultiFor.java
@@ -0,0 +1,22 @@
+package Chapter5.codeMagnets;
+
+public class MultiFor {
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 4; i++){
+            /*if(i == 1){
+                i++;
+            }*/
+            for(int j = 4; j > 2; j--){
+                System.out.println(i + " " + j);
+                if(i == 3 || j == 3){
+
+                }
+            }
+            if(i == 1){
+                i++;
+            }
+
+        }
+    }
+}
