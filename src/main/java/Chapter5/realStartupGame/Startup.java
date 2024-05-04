b7100717e69315b8cb9b3c08e3e77d2352fc9b4d
Pabalberione <pabalberione@gmail.com>
2024-05-04 10:26:49 -0300

    Adding realStartupGame chapter 5 exercise

--- src/main/java/Chapter5/realStartupGame/Startup.java
+++ src/main/java/Chapter5/realStartupGame/Startup.java
@@ -1,22 +1,28 @@
-package Chapter5.simpleStatupGame;
+package Chapter5.realStartupGame;
 
 import java.util.ArrayList;
 
-public class SimpleStartup {
+public class Startup {
     private ArrayList<String> locationCells;
-    private int numofHits = 0;
+    private String name;
 
-    public void setLocationCells(ArrayList<String>locs){
-        locationCells = locs;
+    public void setLocationCells(ArrayList<String> loc){
+        locationCells = loc;
     }
 
-    public String checkYourselfIf(int userInput){//Metodo que chequea si una parte de la startup fue golpeada, recibe por parametro un numero
+    public void setName(String n){
+        name = n;
+    }
+
+    public String checkYourself(String userInput){
         String result = "miss";
         int index = locationCells.indexOf(userInput);
-        if(index>=0){
+        if (index >= 0){
             locationCells.remove(index);
+
             if(locationCells.isEmpty()){
                 result = "kill";
+                System.out.println("Ouch! You sunk " + name + " :( ");
             }else {
                 result = "hit";
             }
